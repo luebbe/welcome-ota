76e4c0b83d60db9eafed5c3c0b237e2de4b95771
Lübbe <luebbe.opensource@gmail.com>
2017-10-26 22:23:45 +0200

    Add example for 128x32 display using the U8g2 display library

--- src/main.cpp
+++ src/main.cpp
@@ -3,48 +3,69 @@
 
 #define FW_VERSION "1.0.1"
 
-// Uncomment this line if you want to see the OTA progress
-// on the OLED display
-#define DISPLAY_SSD1306
+// Uncomment the line corresponding to the OLED driver you want to use to 
+// show the OTA progress on the OLED display. Or none if you want just serial logging
+// #define DISPLAY_SSD1306
+#define DISPLAY_U8G2
 
-#ifdef DISPLAY_SSD1306
-  #define FW_NAME "ota-display-ssd1306"
-  // #define DISPLAY_HEIGHT 32
+const int PIN_SDA = 5;
+const int PIN_SCL = 4;
 
-  // Setup OTA logging via OLED dislpay and Homie logger
-  #include <SSD1306.h>
+#if defined(DISPLAY_SSD1306)
+//
+// Setup OTA logging via SSD1306 OLED display driver and Homie logger
+//
+#define FW_NAME "ota-display-ssd1306"
+#include <SSD1306.h>
 
-  // Put your settings for the I2C bus here
-  const int I2C_DISPLAY_ADDRESS = 0x3c;
-  const int SDA_PIN = 5;
-  const int SCL_PIN = 4;
+// Put your settings for the I2C bus here
+const int I2C_DISPLAY_ADDRESS = 0x3c;
 
-  SSD1306Wire display(I2C_DISPLAY_ADDRESS, SDA_PIN, SCL_PIN);
-  OtaDisplaySSD1306 ota(display, NULL);
+SSD1306Wire display(I2C_DISPLAY_ADDRESS, PIN_SDA, PIN_SCL);
+OtaDisplaySSD1306 ota(display, NULL);
+
+#elif defined(DISPLAY_U8G2)
+//
+// Setup OTA logging via U8G2 OLED display driver and Homie logger
+//
+#define FW_NAME "ota-display-u8g2"
+#include <u8g2lib.h>
+
+const int PIN_RESET = U8X8_PIN_NONE; // The reset pin of the SD1306 is not connected
+
+U8G2_SSD1306_128X32_UNIVISION_F_HW_I2C u8g2(U8G2_R0, PIN_RESET, PIN_SCL, PIN_SDA);
+OtaDisplayU8G2 ota(u8g2, NULL);
+
 #else
-  #define FW_NAME "ota-basic-serial"
-  // Setup OTA logging via Homie logger
-  OtaLogger ota;
+#define FW_NAME "ota-basic-serial"
+//
+// Setup OTA logging via Homie logger
+//
+OtaLogger ota;
 #endif
 
-void setupHandler() {
+void setupHandler()
+{
   // This is called after the MQTT_CONNECTED event
   ota.setup();
 }
 
-void loopHandler() {
+void loopHandler()
+{
   // This is only called by Homie when WiFi is connected
   ota.loop();
 }
 
-void setup() {
+void setup()
+{
   Serial.begin(SERIAL_SPEED);
-  Serial << endl << endl;
+  Serial << endl
+         << endl;
 
   // show state and last reset reason
   welcome();
 
-#ifdef DISPLAY_SSD1306
+#if defined(DISPLAY_SSD1306)
   // initlalize display
   display.init();
   display.clear();
@@ -52,8 +73,11 @@ void setup() {
   display.setColor(WHITE);
   display.setFont(ArialMT_Plain_10);
   display.setTextAlignment(TEXT_ALIGN_CENTER);
-  display.drawString(64, DISPLAY_HEIGHT/2, (String)FW_NAME + " " + (String)FW_VERSION);
+  display.drawString(64, DISPLAY_HEIGHT / 2, (String)FW_NAME + " " + (String)FW_VERSION);
   display.display();
+#elif defined(DISPLAY_U8G2)
+  u8g2.begin();
+#else
 #endif
 
   Homie_setFirmware(FW_NAME, FW_VERSION);
@@ -68,6 +92,7 @@ void setup() {
   Homie.setup();
 }
 
-void loop() {
-   Homie.loop();
+void loop()
+{
+  Homie.loop();
 }
